854d9b9b7b99cbfe48273069d68664ec5c406631
Vaishnavi Buddharaju <vaishnavi.buddharaju@siriusxm.com>
2023-08-29 03:26:50 -0400

    cdktf stack

---
+++ cdk-tf/main.ts
@@ -0,0 +1,54 @@
+import { Construct } from "constructs";
+import { App, TerraformOutput, TerraformStack } from "cdktf";
+import { DatabricksProvider } from '@cdktf/provider-databricks/lib/provider';
+import { DataDatabricksCurrentUser } from '@cdktf/provider-databricks/lib/data-databricks-current-user';
+import { Notebook } from '@cdktf/provider-databricks/lib/notebook';
+import { Job } from '@cdktf/provider-databricks/lib/job';
+import * as vars from "./vars";
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    new DatabricksProvider(this, "databricksAuth", {})
+
+    const currentUser = new DataDatabricksCurrentUser(this, "currentUser", {});
+
+    // Define the notebook.
+    const notebook = new Notebook(this, "notebook", {
+      path: `${currentUser.home}/CDKTF/${vars.resourcePrefix}-notebook.py`,
+      language: "PYTHON",
+      contentBase64: Buffer.from("display(spark.range(10))", "utf8").toString("base64")
+    });
+
+    // Define the job to run the notebook.
+    const job = new Job(this, "job", {
+      name: `${vars.resourcePrefix}-job`,
+      newCluster: {
+        numWorkers: vars.numWorkers,
+        sparkVersion: vars.sparkVersion,
+        nodeTypeId: vars.nodeTypeId
+      },
+      notebookTask: {
+        notebookPath: `${currentUser.home}/CDKTF/${vars.resourcePrefix}-notebook.py`
+      },
+      emailNotifications: {
+        onSuccess: [ currentUser.userName ],
+        onFailure: [ currentUser.userName ]
+      }
+    });
+
+    // Output the notebook and job URLs.
+    new TerraformOutput(this, "Notebook URL", {
+      value: notebook.url
+    });
+
+    new TerraformOutput(this, "Job URL", {
+      value: job.url
+    });
+  }
+}
+
+const app = new App();
+new MyStack(app, "cdktf-demo");
+app.synth();
---
+++ cdk-tf/vars.ts
@@ -0,0 +1,4 @@
+export const resourcePrefix = "cdktf-demo"
+export const numWorkers     = 1
+export const sparkVersion   = "10.4.x-scala2.12"
+export const nodeTypeId     = "i3.xlarge"
