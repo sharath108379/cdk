a021759d729206278e6b9dbf49406151a9d14ed2
sharath108379 <sharath108379>
2023-08-29 12:16:08 +0000

    db tst

--- cdk-tf/vars.ts
+++ cdk-tf/vars.ts
@@ -1,4 +1,5 @@
 export const resourcePrefix = "cdktf-demo"
 export const numWorkers     = 1
-export const sparkVersion   = "10.4.x-scala2.12"
-export const nodeTypeId     = "i3.xlarge"
+export const sparkVersion   = "12.2.x-scala2.12"
+export const nodeTypeId = "i3.xlarge"
+export const userEmail = "sharath.kolachalam@marlabs.com"
